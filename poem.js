7504390d126363bcfa5f72c193761da174552e8b
kumudj23 <34874012+kumudj23@users.noreply.github.com>
2018-08-16 15:40:56 -0400

    Update poem.js

--- poem.js
+++ poem.js
@@ -2,13 +2,44 @@ function Get(yourUrl){
     var Httpreq = new XMLHttpRequest(); // a new request
     Httpreq.open("GET",yourUrl, false);
     Httpreq.send(null);
-    //console.log(typeof(Httpreq.responseText));
-    console.log(Httpreq.responseText);
-    console.log((Httpreq.responseText).length);
-  return (Httpreq.responseTex);
+    // console.log(typeof(Httpreq.responseText));
+    //console.log(Httpreq.responseText);
+    // console.log((Httpreq.responseText).length);
+    var newStr = "";
+    var adding = false;
+    // var docstrings = false;
+    var i;
+    for (i = 0; i < Httpreq.responseText.length; i++) {
+      var ch = Httpreq.responseText[i];
+      // console.log(ch)
+      if (ch == "{"){
+        // if (docstrings == false){
+        //   newStr += "'''";
+        //   docstrings = true;
+        // }
+        newStr += ch;
+        adding = true;
+      }
+      else if (ch == "}"){
+        newStr += ch;
+        newStr += ch;
+        // newStr += "'''"
+        adding = false;
+        break;
+      }
+      else if(adding == true){
+        newStr += ch;
+      }
+    }
+  //console.log(newStr)
+  return (newStr);
 }
   var item = Get("https://www.poemist.com/api/v1/randompoems");
-  document.getElementById("poem").innerHTML = item;
+  //console.log(item)
+  var object = JSON.parse(item)
+  document.getElementById("title").innerHTML = object.title;
+  document.getElementById("content").innerHTML = object.content;
+
  // var  item = JSON.stringify(item);
 
 
